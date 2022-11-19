a3d7680653298f4bf287072beed13784f9d7f382
dhiransapkota45 <dhiransapkota55@gmail.com>
2022-11-19 22:07:09 +0545

    validation added for signup

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import { FcGoogle } from "react-icons/fc"
 import { FaFacebookF } from "react-icons/fa"
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+const schema = yup.object({
+    firstname: yup.string().required("firstname is required"),
+    lastname: yup.string().required("lastname is required"),
+    email: yup.string().email("field should contain a valid email").required("email is required"),
+    password: yup.string().required("password is required"),
+    cpassword: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match')
+}).required();
 
 const Signup = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm({
+        resolver: yupResolver(schema)
+    });
+    const onSubmit = (data) => { console.log(data) }
     return (
         <div className=" bg-hero-pattern h-[800px] w-full flex justify-center ">
             <div className='grid grid-rows-2 w-4/5'>
@@ -15,29 +30,38 @@ const Signup = () => {
                         <button className='flex p-2 bg-white text-black gap-3 w-32 items-center font-semibold'><FaFacebookF className='text-blue-600' />Facebook</button>
                     </div>
 
-                    <form className=' w-full sm:w-[420px]'>
+                    <form onSubmit={handleSubmit(onSubmit)} className=' w-full sm:w-[420px]'>
                         <div className=' my-4 flex justify-between'>
                             <div className=''>
                                 <div className='font-bold mb-2'>First Name</div>
-                                <input className='  p-2 text-black outline-none' type="password" placeholder='First Name' />
+                                <input {...register("firstname")} className='  p-2 text-black outline-none' type="text" placeholder='First Name' />
+                                {errors?.firstname && <p className=' text-red-300 font-bold'>*{errors.firstname.message}</p>}
                             </div>
                             <div>
                                 <div className='font-bold mb-2'>Last Name</div>
-                                <input className='  p-2 text-black outline-none' type="password" placeholder='Last Name' />
+                                <input {...register("lastname")} className='  p-2 text-black outline-none' type="text" placeholder='Last Name' />
+                                {errors?.lastname && <p className=' text-red-300 font-bold'>*{errors.lastname.message}</p>}
                             </div>
                         </div>
                         <div className='mb-4'>
                             <div className=' font-bold mb-2 '>Email</div>
-                            <input className=' w-full p-2 text-black outline-none ' type="email" placeholder='Email' />
+                            <input {...register("email")} className=' w-full p-2 text-black outline-none ' type="email" placeholder='Email' />
+                            {errors?.email?.type==="required" && <p className=' text-red-300 font-bold'>*{errors.email.message}</p>}
+                            {errors?.email?.type==="email" && <p className=' text-red-300 font-bold'>*{errors.email.message}</p>}
+
                         </div>
                         <div>
                             <div className='font-bold mb-2'>Password</div>
-                            <input className=' w-full p-2 text-black outline-none' type="password" placeholder='Password' />
+                            <input {...register("password")} className=' w-full p-2 text-black outline-none' type="password" placeholder='Password' />
+                            {errors?.password?.type==="required" && <p className=' text-red-300 font-bold'>*{errors.password.message}</p>}
+
                         </div>
 
-                        <div>
+                        <div className=' mt-4'>
                             <div className='font-bold mb-2'>Enter Password Again</div>
-                            <input className=' w-full p-2 text-black outline-none' type="password" placeholder='Password' />
+                            <input {...register("cpassword")} className=' w-full p-2 text-black outline-none' type="password" placeholder='Password' />
+                            {errors?.cpassword?.type==="oneOf" && <p className=' text-red-300 font-bold'>*{errors.cpassword.message}</p>}
+                            
                         </div>
 
                         <div className=' mt-4 flex justify-between'>

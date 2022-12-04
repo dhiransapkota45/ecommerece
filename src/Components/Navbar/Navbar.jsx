4a2d2f84cad8689dbe171c0c75f2e8aab75cab8b
dhiransapkota45 <dhiransapkota55@gmail.com>
2022-12-04 13:14:27 +0545

    sidenav added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,39 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from "./assets/Logo.png"
+import Hamiicon from "./assets/Vector.png"
 import { NavLink } from 'react-router-dom'
 import { FiShoppingCart } from "react-icons/fi"
 import { AiOutlineHeart } from "react-icons/ai"
 import { CgProfile } from "react-icons/cg"
 import { FiSearch } from "react-icons/fi"
 
+import Sidenav from './Sidenav'
+
 const Navbar = () => {
+  const [hamburger, setHamburger] = useState(false)
   return (
     <div className=' z-30 bg-white w-full shadow-lg sticky top-0 flex justify-center'>
-      <div className='w-4/5  p-4 flex justify-between'>
+      <div className='w-4/5  p-4 flex justify-between items-center'>
         <div className='flex items-center'>
           <img src={Logo} alt="" />
           <div className=' font-bold ml-4 w-36'>mangcoding Store</div>
         </div>
-        <div className=' flex  gap-6 items-center font-semibold '>
-          <NavLink to="/" > Home</NavLink>
-          <NavLink activeClassName="activenavlink" to="/shop"> Shop</NavLink>
-          <NavLink to="/about"> About Us</NavLink>
-          <NavLink to="/contact"> Contact</NavLink>
-          <NavLink to="/cart" >
+        <div id='mainnav' className='  hidden xl:flex  gap-6 items-center font-semibold '>
+          <NavLink className=" " to="/" > Home</NavLink>
+          <NavLink className=" " activeClassName="activenavlink" to="/shop"> Shop</NavLink>
+          <NavLink className=" " to="/about"> About Us</NavLink>
+          <NavLink className=" " to="/contact"> Contact</NavLink>
+          <NavLink className=" " to="/cart" >
             <FiShoppingCart className=' text-xl' />
           </NavLink>
-          <NavLink to="/wishlist" >
+          <NavLink className=" " to="/wishlist" >
             <AiOutlineHeart className=' text-xl' />
           </NavLink>
-          <NavLink to="/account">
+          <NavLink className=" " to="/account">
             <CgProfile className=' text-xl ' />
           </NavLink>
           <div className=' flex items-center mx-6 border-2 p-2 rounded-md'>
-            {/* <input className=' border-none overflow-hidden outline-none border w-52  ' type="text" placeholder='Search' /> */}
+            <input className=' border-none overflow-hidden outline-none border w-52  ' type="text" placeholder='Search' />
             <FiSearch className='text-md  mr-4 text-gray-600' />
           </div>
         </div>
+
+        <button onClick={() => setHamburger(!hamburger)} className=' w-6 h-6  relative flex flex-col  gap-1 xl:hidden'>
+          <img className={`${hamburger ? " rotate-45 absolute top-1" : " rotate-0"} duration-300`} src={Hamiicon} alt="" />
+          <img className={`${hamburger ? "  hidden" : "block"} duration-300`} src={Hamiicon} alt="" />
+          <img className={`${hamburger ? " -rotate-45 absolute   top-1" : "rotate-0"} duration-300`} src={Hamiicon} alt="" />
+        </button>
       </div>
+
+      <Sidenav setHamburger={setHamburger} hamburger={hamburger} />
     </div>
   )
 }
---
+++ src/Components/Navbar/Sidenav.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { GiCrossedBones } from "react-icons/gi"
+import { NavLink } from 'react-router-dom'
+import { FiShoppingCart } from "react-icons/fi"
+import { AiOutlineHeart } from "react-icons/ai"
+import { CgProfile } from "react-icons/cg"
+
+const Sidenav = ({hamburger, setHamburger}) => {
+    return (
+        <>
+            {hamburger && <div className=' fixed z-40 top-0 w-full h-screen bg-black bg-opacity-60'></div>}
+            {
+                <div id='offcanvas' className={` fixed top-0 w-full sm:w-96 z-50 h-full  bg-gradient-to-t bg-white ${hamburger ? "left-0" : " -left-full sm:-left-96 "} duration-500`}>
+                    <div className=' h-16 px-2 flex justify-end'>
+                        <button onClick={() => setHamburger(!hamburger)}>
+                            <GiCrossedBones />
+                        </button>
+                    </div>
+
+                    <div className='flex divide-y flex-col items-center py-5 px-3  font-semibold '>
+                        <NavLink className=" p-4 w-full text-center " to="/" > Home</NavLink>
+                        <NavLink className=" p-4 w-full text-center" to="/shop"> Shop</NavLink>
+                        <NavLink className=" p-4 w-full text-center" to="/about"> About Us</NavLink>
+                        <NavLink className=" p-4 w-full text-center" to="/contact"> Contact</NavLink>
+                        <NavLink className=" p-4 w-full flex justify-center" to="/cart"  >
+                            <FiShoppingCart className=' text-xl ' />
+                        </NavLink>
+                        <NavLink className=" p-4 w-full flex justify-center" to="/wishlist"  >
+                            <AiOutlineHeart className=' text-xl' />
+                        </NavLink>
+                        <NavLink className=" p-4 w-full flex justify-center" to="/account">
+                            <CgProfile className=' text-xl ' />
+                        </NavLink>
+                    </div>
+                </div>
+            }
+        </>
+    )
+}
+
+export default Sidenav
